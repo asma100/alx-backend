514f99aae251187fafaf03b1b917272e97caca26
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-08-27 13:11:28 +0000

    task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""task 2"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """config"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    user_language =
+    request.accept_languages.best_match(babel.supported_locales)
+    return user_language
+
+
+@app.route('/')
+def Welcome():
+    """welcome"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
